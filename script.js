2d7f91cae0f22cc144cd0e72dd2ff06bfc136793
Himanshu Joshi <himanshujoshi3304@gmail.com>
2024-01-13 04:33:51 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function openNav() {
+    let nav = document.getElementById('nav');
+    nav.style.left = "0px"
+}
+
+function closeNav() {
+    let nav = document.getElementById('nav');
+    nav.style.left = "-1000px"
+}
+
+function load() {
+    let navIcon = document.getElementById('nav-icon');
+    navIcon.addEventListener("click", openNav); 
+    
+    let closeIcon = document.getElementById('nav-close-icon');
+    closeIcon.addEventListener("click", closeNav); 
+    
+}
+
+document.addEventListener("DOMContentLoaded", load)
